f8ada134e9289ccc92200040e7a7f952d977c3c5
TanushreeVedula14 <tanushree.vedula14@gmail.com>
2018-11-13 15:50:41 +0530

    "ads2_m2_a1_1: 6/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ m-2/2.1 CC_Percolation/2.1 CC_Percolation/CC.java
@@ -0,0 +1,75 @@
+class CC 
+{
+	boolean marked[];
+	int id[];
+	int size[];
+	int count;
+	
+	CC(Graph G)
+	{
+		marked = new boolean[G.V()];
+        id = new int[G.V()];
+        size = new int[G.V()];
+        for (int v = 0; v < G.V(); v++) 
+        {
+            if (!marked[v]) 
+            {
+                dfs(G, v);
+                count++;
+            }
+        }
+	}
+	
+	void dfs(Graph G, int v) 
+	{
+		marked[v] = true;
+        id[v] = count;
+        size[count]++;
+        for (int w : G.adj(v))
+        {
+            if (!marked[w])
+            {
+                dfs(G, w);
+            }
+        }
+    }
+	
+	boolean connected(int v, int w)
+	{
+        validateVertex(v);
+        validateVertex(w);
+        return id(v) == id(w);
+    }
+	
+	void validateVertex(int v)
+	{
+        int V = marked.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+	
+	int id(int v) 
+	{
+        validateVertex(v);
+        return id[v];
+    }
+	
+	int size(int v) 
+	{
+        validateVertex(v);
+        return size[id[v]];
+    }
+	
+	int count() 
+	{
+        return count;
+    }
+	
+	void disp()
+	{
+		if(count == 1)
+			System.out.println(true);
+		else
+			System.out.println(false);
+	}
+}
---
+++ m-2/2.1 CC_Percolation/2.1 CC_Percolation/Solution.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution 
+{
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws IOException 
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Graph G = new Graph(br);
+		CC c = new CC(G);
+		
+		c.disp();		
+	}
+
+}
